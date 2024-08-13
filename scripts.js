76fd0dd85823448557d897ddb7baadfbe83f7c94
ivisonalvesdev <devivisonalves@gmail.com>
2024-08-13 07:29:31 -0300

    corrigido

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function generateNumber() {
+    const min = document.querySelector('.input-min').value;
+    const max = document.querySelector('.input-max').value;
+
+    console.log(min, max);
+}
+
+document.querySelector('button').addEventListener('click', generateNumber);
